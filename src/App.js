3ba386f52a9d9d6ece3d47b4fbba0c68ea5a5533
Thud <thuddesigns@gmail.com>
2023-03-24 17:20:46 -0700

    testt

--- src/App.js
+++ src/App.js
@@ -2,26 +2,25 @@ import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from "./components/Home"
 import Store from "./components/Store"
-import IPFS from 'ipfs-http-client';
+import { create as createIPFS } from 'ipfs-http-client';
 
-const ipfs = new IPFS({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
 
 async function getIpfsContentAddress() {
+  const ipfs = await createIPFS();
   const domainName = 'the-remedy.4everland.app'; // Replace with your domain name
   const path = '/'; // Replace with the path to your content
   const result = await ipfs.name.resolve(`${domainName}${path}`);
-  const ipfsContentAddress = result.path.replace('/ipfs/', '');
+  const ipfsContentAddress = result?.path?.replace('/ipfs/', '');
   return ipfsContentAddress;
 }
 
 function App() {
   const [ipfsContentAddress, setIpfsContentAddress] = useState('');
 
-  console.log(ipfsContentAddress)
-
   useEffect(() => {
     async function fetchIpfsContentAddress() {
       const contentAddress = await getIpfsContentAddress();
+      console.log(contentAddress)
       setIpfsContentAddress(contentAddress);
     }
     fetchIpfsContentAddress();
