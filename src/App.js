c530d2748912be6beea4ad45eeba95bae3ccb632
Thud <thuddesigns@gmail.com>
2023-04-28 16:01:31 -0700

    fix routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import Home from "./routes/Home"
 import Store from "./routes/Store"
 import Education from "./routes/Education"
 import Legal from "./routes/Legal"
 import Contact from "./routes/Contact"
 import Terms from "./routes/Terms"
+import { useEffect } from 'react';
 
 function App() {
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (window.location.search.includes("location")) {
+      navigate(window.location.search.split("location=")[1])
+    }
+  }, [window])
 
   return (
     <Routes>
